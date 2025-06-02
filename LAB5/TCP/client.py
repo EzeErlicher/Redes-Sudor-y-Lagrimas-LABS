faa5a4955b7b475b821a1827e560d3ba68b412ec
Juan Badariotti <juan.badariotti@mi.unc.edu.ar>
2025-06-02 15:03:49 -0300

    1) Logs opcionales con argumento "-l" + simplificación

--- LAB5/TCP/client.py
+++ LAB5/TCP/client.py
@@ -1,7 +1,9 @@
 import socket
 import struct
 from datetime import datetime
+import sys
 
+# IP local del servidor
 HOST = 'localhost'
 PORT = 9090
 
@@ -19,43 +21,46 @@ except ConnectionRefusedError:
 
 print("Conexión con el servidor exitosa.")
 
-# Archivo de logueo de la recepción de paquetes
-filePath = f"logs/clientTCPLog{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
-logFile = open(filePath, "w",encoding="utf-8")
-print(f"Log de ejecución guardado en archivo '{filePath}'")
+# Revisar si se pasa el argumento -l
+logToFile = "-l" in sys.argv
+if logToFile:
+    filePath = f"logs/clientTCPLog_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+    logFile = open(filePath, "w", encoding="utf-8")
+    print(f"Log de ejecución guardado en archivo '{filePath}'")
+
+# Recibe exactamente 'num_bytes' bytes del socket.
+def recv_exact(sock, num_bytes):
+    buffer = b''
+    while len(buffer) < num_bytes:
+        try:
+            chunk = sock.recv(num_bytes - len(buffer))
+            if not chunk:
+                return None  # Desconexión o paquete incompleto
+            buffer += chunk
+        except (ConnectionAbortedError, ConnectionResetError):
+            return None      # Error de conexión
+    return buffer
 
 while packetCount < totalPackets:
-    try:
-        # Recibe cabecera (4 bytes)
-        header = b''
-        while len(header) < 4:
-            data = clientSocket.recv(4 - len(header))
-            if not data:
-                print("ERROR: Header del paquete invalido")
-                break
-            header += data
-        msg_length = struct.unpack('!I', header)[0]  # Decodifica longitud
-        
-        # Recibe el cuerpo del paquete con la cantidad de bytes definidos por el header
-        body = b''
-        while len(body) < msg_length:
-            data = clientSocket.recv(msg_length - len(body))
-            if not data:
-                print("ERROR: Cuerpo del paquete invalido")
-                break
-            body += data
-        
-        if len(body) < msg_length:
-            break  # Cuerpo incompleto
-        
-        rx_message = body.decode('utf-8')
-        #print(f"  Server: '{rx_message}'")
-        logFile.write(f"Mensaje {packetCount+1} recibido - '{rx_message}' - {datetime.now()}\n")
-        packetCount += 1
-
-    except (ConnectionAbortedError, ConnectionResetError):
-        print("ERROR: Se cayó el server.")
+    # Recibe cabecera (4 bytes)
+    header = recv_exact(clientSocket, 4)
+    if header is None:
+        print(f"ERROR: Falló la recepción del header del paquete {packetCount+1}")
         break
+    msg_length = struct.unpack('!I', header)[0]  # Decodifica longitud
+    
+    # Recibe el cuerpo del paquete con la cantidad de bytes definidos por el header
+    body = recv_exact(clientSocket, msg_length)
+    if body is None:
+        print(f"ERROR: Falló la recepción del cuerpo del paquete {packetCount+1}")
+        break
+
+    rx_message = body.decode('utf-8')
+    logMessage = f"Mensaje {packetCount+1} recibido - '{rx_message}' - {datetime.now()}\n"
+    logFile.write(logMessage) if logToFile else print(logMessage.strip())
+
+    packetCount += 1
 
 print(f"Total de paquetes recibidos: {packetCount}")
 clientSocket.close()
+if logToFile: logFile.close()
--- LAB5/TCP/server.py
+++ LAB5/TCP/server.py
@@ -2,43 +2,45 @@ import socket
 import struct
 import time
 from datetime import datetime
+import sys
 
 # IP local de la computadora
 HOST = 'localhost'
 PORT = 9090
 
 groupName = "Redes, sudor y lágrimas"
-totalPackets = 100    # Cantidad de paquetes a transmitir
-sleepTime = 1         # Espera en segundos entre el envío de un paquete y el siguiente
+totalPackets = 10    # Cantidad de paquetes a transmitir
+sleepTime = 0.5        # Espera en segundos entre el envío de un paquete y el siguiente
 
 # Crea el socket TCP por IPv4 y lo asocia a la dirección especificada
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind((HOST, PORT))
-# Escucha solicitudes de conexión con maximo 1 conexión esperando en cola
-serverSocket.listen(1)
+serverSocket.listen(1)  # Escucha solicitudes de conexión con maximo 1 conexión esperando en cola
 
 print(f"Servidor escuchando en {HOST}:{PORT}")
 
 connectionSocket, addr = serverSocket.accept()
 print(f"  Cliente conectado en {addr[0]}:{addr[1]}")
 
-# Archivo de logueo de los envios de paquetes
-filePath = f"logs/serverTCPLog{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
-logFile = open(filePath, "w",encoding="utf-8")
-print(f"  Log de ejecución guardado en archivo '{filePath}'")
+# Revisar si se pasa el argumento -l
+logToFile = "-l" in sys.argv
+if logToFile:
+    # Archivo de logueo de los envios de paquetes
+    filePath = f"logs/serverTCPLog_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+    logFile = open(filePath, "w", encoding="utf-8")
+    print(f"  Log de ejecución guardado en archivo '{filePath}'")
 
-for i in range(totalPackets):
-    tx_message = f"{groupName} - {i+1}"
+for i in range(1, totalPackets + 1):
+    tx_message = f"{groupName} - {i}"
+    # Paquete = Cabecera + Contenido
+    packet = struct.pack('!I', len(tx_message.encode())) + tx_message.encode()
 
-    body = tx_message.encode()
-    header = struct.pack('!I', len(body))
-    packet = header + body
+    connectionSocket.sendall(packet)
+    logMessage = f"Mensaje {i} enviado - '{tx_message}' - {datetime.now()}\n"
 
-    connectionSocket.sendall(packet)  # Envia cabecera + cuerpo
-    logFile.write(f"Mensaje {i+1} enviado - '{tx_message}' - {datetime.now()}\n")
+    logFile.write(logMessage) if logToFile else print(logMessage.strip())
 
     time.sleep(sleepTime)
 
 print(f"  {totalPackets} paquetes enviados con exito al cliente.\nCerrrando ejecución...")
 connectionSocket.close()
-logFile.close()
