c792f6a5ec14e730f3ffbedf1e47514bb1f71416
Juan Badariotti <juan.badariotti@mi.unc.edu.ar>
2025-06-02 12:01:16 -0300

    1) Scripts adaptados para la prueba

--- LAB5/TCP/client.py
+++ LAB5/TCP/client.py
@@ -1,7 +1,15 @@
 import socket
+import struct
 
 HOST = 'localhost'
 PORT = 9090
+
+groupName = "Redes, sudor y lágrimas"
+packetCount = 0           # Contador de paquetes recibidos
+totalPackets = 100          # Cantidad de paquetes a recibir
+
+logToConsole = True
+logToFile = False
 
 # Crea el socket TCP y la solicitud de conexión al servidor
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,26 +19,39 @@ except ConnectionRefusedError:
     print("ERROR: Falló la conexión porque el servidor no está disponible.")
     exit()
 
-# Recibe el mensaje inicial del servidor
-rx_message = clientSocket.recv(1024)
-print(f"\nServer: '{rx_message.decode()}'")
+print("Conexión con el servidor exitosa.")
 
-while True:
-    # Envía el input del usuario al servidor
-    tx_message = input("   Escriba su mensaje: ")
-    clientSocket.send(tx_message.encode())
-
-    # Espera la respuesta del servidor, en caso de falla cierra el socket
+while packetCount < totalPackets:
     try:
-        rx_message = clientSocket.recv(1024)
-        print(f"Server: '{rx_message.decode()}'")
+        # Recibe cabecera (4 bytes)
+        header = b''
+        while len(header) < 4:
+            data = clientSocket.recv(4 - len(header))
+            if not data:
+                print("ERROR: Header del paquete invalido")
+                break
+            header += data
+        msg_length = struct.unpack('!I', header)[0]  # Decodifica longitud
+        
+        # Recibe el cuerpo del paquete con la cantidad de bytes definidos por el header
+        body = b''
+        while len(body) < msg_length:
+            data = clientSocket.recv(msg_length - len(body))
+            if not data:
+                print("ERROR: Cuerpo del paquete invalido")
+                break
+            body += data
+        
+        if len(body) < msg_length:
+            break  # Cuerpo incompleto
+        
+        rx_message = body.decode('utf-8')
+        #print(f"  Server: '{rx_message}'")
+        packetCount += 1
+
     except (ConnectionAbortedError, ConnectionResetError):
         print("ERROR: Se cayó el server.")
         break
 
-    # Si se envió un comando de cierre o apagado, cierra el socket
-    if "close" in tx_message or "shutdown" in tx_message:
-        break
-
-print("")
+print(f"Total de paquetes recibidos: {packetCount}")
 clientSocket.close()
--- LAB5/TCP/server.py
+++ LAB5/TCP/server.py
@@ -1,58 +1,33 @@
 import socket
+import struct
 from datetime import datetime
 
+# IP local de la computadora
 HOST = 'localhost'
 PORT = 9090
 
-# Cierre del socket de conexión con el cliente
-def closeSocket(connectionSocket,clientCount):
-    connectionSocket.send("Desconexión exitosa".encode())
-    connectionSocket.close()
-    print(f" Cliente {clientCount} se ha desconectado exitosamente.")
+groupName = "Redes, sudor y lágrimas"
+totalPackets = 100    # Cantidad de paquetes a transmitir
 
 # Crea el socket TCP por IPv4 y lo asocia a la dirección especificada
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind((HOST, PORT))
-
 # Escucha solicitudes de conexión con maximo 1 conexión esperando en cola
 serverSocket.listen(1)
 
 print(f"Servidor escuchando en {HOST}:{PORT}")
 
-clientCount = 0           # Contador de conexiones recibidas
-waitingForClient = True   # Variable auxiliar para aceptar nuevas conexiones
+connectionSocket, addr = serverSocket.accept()
+print(f"  Cliente conectado en {addr[0]}:{addr[1]}")
 
-while True:
+for i in range(totalPackets):
+    tx_message = f"{groupName} - {i+1}"
 
-    # Espera nuevo cliente
-    if waitingForClient:
-        # Acepta al cliente creando su socket especifico
-        connectionSocket, addr = serverSocket.accept()
+    body = tx_message.encode()
+    header = struct.pack('!I', len(body))
+    packet = header + body
 
-        clientCount += 1
-        waitingForClient = False
-        print(f"\n Cliente {clientCount} conectado en {addr[0]}:{addr[1]}")
+    connectionSocket.sendall(packet)  # Envia cabecera + cuerpo
 
-        # Envía un mensaje de confirmación de conexión exitosa
-        tx_message = f"Bienvenido cliente {clientCount}! Felicitaciones por una conexión exitosa con el servidor. La hora es {datetime.now().strftime("%H:%M")}"
-        connectionSocket.send(tx_message.encode())
-
-    # Espera a un mensaje del cliente, en caso de falla cierra el socket de conexión
-    try:
-        rx_message = connectionSocket.recv(1024).decode()
-    except (ConnectionResetError, ConnectionAbortedError):
-        print(f" Cliente {clientCount} se desconectó inesperadamente.")
-        connectionSocket.close()
-        waitingForClient = True
-        continue
-
-    if "close" in rx_message.lower():               # Comando para cerrar la conexión
-        closeSocket(connectionSocket,clientCount)
-        waitingForClient = True
-    elif "shutdown" in rx_message.lower():          # Comando para cerrar el servidor
-        closeSocket(connectionSocket,clientCount)
-        print("\nApagando servidor...")
-        break
-    else :                                          # Mensaje del cliente
-        print(f"	Mensaje recibido de cliente {clientCount}: '{rx_message}'")
-        connectionSocket.send(f'Mensaje "{rx_message}" recibido!'.encode())
+print(f"  {totalPackets} paquetes enviados con exito al cliente.\nCerrrando ejecución...")
+connectionSocket.close()
